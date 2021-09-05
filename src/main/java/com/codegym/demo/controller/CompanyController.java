f5f70f0a50290beee59f209ddcbe170dd5d1903a
tiennv0608 <tiennv0608@gmail.com>
2021-09-05 10:54:53 +0700

    done edit company

--- src/main/java/com/codegym/demo/controller/CompanyController.java
+++ src/main/java/com/codegym/demo/controller/CompanyController.java
@@ -5,12 +5,10 @@ import com.codegym.demo.service.company.ICompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/companies")
@@ -26,5 +24,54 @@ public class CompanyController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(companies, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Company> edit(@PathVariable Long id, @RequestBody Company company) {
+        Optional<Company> companyOptional = companyService.findById(id);
+        if (!companyOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        company.setId(id);
+        company.setEmail(companyOptional.get().getEmail());
+        company.setType(companyOptional.get().getType());
+        if (company.getCompanyName().trim().equals("")) {
+            company.setCompanyName(companyOptional.get().getCompanyName());
+        }
+        if (company.getShortName().trim().equals("")) {
+            company.setShortName(companyOptional.get().getShortName());
+        }
+        if (company.getCompanyCode().trim().equals("")) {
+            company.setCompanyName(companyOptional.get().getCompanyCode());
+        }
+        if (company.getAddress().trim().equals("")) {
+            company.setAddress(companyOptional.get().getAddress());
+        }
+        if (company.getBranch().trim().equals("")) {
+            company.setBranch(companyOptional.get().getBranch());
+        }
+        if (company.getPassword().trim().equals("")) {
+            company.setPassword(companyOptional.get().getPassword());
+        }
+        if (company.getImage().trim().equals("")) {
+            company.setImage(companyOptional.get().getImage());
+        }
+        if (company.getLinkGoogle().trim().equals("")) {
+            company.setLinkGoogle(companyOptional.get().getLinkGoogle());
+        }
+        if (company.getWebsite().trim().equals("")) {
+            company.setWebsite(companyOptional.get().getWebsite());
+        }
+        if (company.getDescription().trim().equals("")) {
+            company.setDescription(companyOptional.get().getDescription());
+        }
+        if (company.getPhone().trim().equals("")) {
+            company.setPhone(companyOptional.get().getPhone());
+        }
+        if (company.getNumberOfStaff() == 0) {
+            company.setNumberOfStaff(companyOptional.get().getNumberOfStaff());
+        }
+        companyService.save(company);
+        return new ResponseEntity<>(company, HttpStatus.OK);
     }
 }
