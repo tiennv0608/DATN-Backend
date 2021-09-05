d6567bdcff8d77f2ad4a075454430c66dda3d3fa
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-09-05 10:03:29 +0700

    back-end user

---
+++ src/main/java/com/codegym/demo/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.codegym.demo.controller;
+
+import com.codegym.demo.model.User;
+import com.codegym.demo.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Iterable<User>> getAll() {
+        Iterable<User> userIterable = userService.findAll();
+        return new ResponseEntity<>(userIterable, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        Optional<User> userOptional = userService.findById(id);
+        if (!userOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> editUser(@PathVariable Long id, @RequestBody User user){
+        Optional< User> userOptional = userService.findById(id);
+        if(!userOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if(user.getName().trim().equals("")){
+            user.setName(userOptional.get().getName());
+        }
+        user.setId(id);
+        return new ResponseEntity<>(userService.save(user), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteById(@PathVariable Long id){
+        Optional<User> userOptional = userService.findById(id);
+        if(!userOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        userService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/codegym/demo/service/user/IUserService.java
@@ -0,0 +1,7 @@
+package com.codegym.demo.service.user;
+
+import com.codegym.demo.model.User;
+import com.codegym.demo.service.IGeneralService;
+
+public interface IUserService extends IGeneralService<User> {
+}
