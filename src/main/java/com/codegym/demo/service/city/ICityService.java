11057104af9a81968f2a8076c36f811d5e101923
vuongvantu <vuongvantu2001x@gmail.com>
2021-09-06 16:42:11 +0700

    add-city

---
+++ src/main/java/com/codegym/demo/service/city/ICityService.java
@@ -0,0 +1,7 @@
+package com.codegym.demo.service.city;
+
+import com.codegym.demo.model.City;
+import com.codegym.demo.service.IGeneralService;
+
+public interface ICityService extends IGeneralService<City> {
+}
