4c527fb5581e8d0911eba0bf5e4224a73107e7cc
admin <dongdd159@gmail.com>
2021-09-07 13:56:30 +0700

    complete search-bar

--- src/main/java/com/codegym/demo/service/post/IPostService.java
+++ src/main/java/com/codegym/demo/service/post/IPostService.java
@@ -1,8 +1,16 @@
 package com.codegym.demo.service.post;
 
+import com.codegym.demo.dto.response.PostResponse;
 import com.codegym.demo.model.Post;
 import com.codegym.demo.service.IGeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IPostService extends IGeneralService<Post> {
     Iterable<Post> findAllByIdCompany(Long id);
+    PostResponse convert (Post post);
+    List<Post> searchJob(String keyword, Long cat_id, Long city_id, double salary1, double salary2);
+    Page<PostResponse> findAllPage(Pageable pageable);
 }
--- src/main/java/com/codegym/demo/service/post/PostService.java
+++ src/main/java/com/codegym/demo/service/post/PostService.java
@@ -1,11 +1,16 @@
 package com.codegym.demo.service.post;
 
+import com.codegym.demo.dto.response.PostResponse;
 import com.codegym.demo.model.Post;
 import com.codegym.demo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
+
 @Service
 public class PostService implements IPostService{
     @Autowired
@@ -34,4 +39,78 @@ public class PostService implements IPostService{
     public Iterable<Post> findAllByIdCompany(Long id) {
         return postRepository.findAllByIdCompany(id);
     }
+    @Override
+    public PostResponse convert(Post post) {
+        PostResponse postResponse = new PostResponse();
+        postResponse.setId(post.getId());
+        postResponse.setAddress(post.getAddress());
+        postResponse.setCompanyname(post.getCompany().getCompanyName());
+        postResponse.setTittle(post.getTitle());
+        postResponse.setExpireddate(post.getExpiredDate());
+        postResponse.setSalary(post.getSalary());
+        return postResponse;
+    }
+
+    @Override
+    public List<Post> searchJob(String keyword, Long cat_id, Long city_id, double salary1, double salary2) {
+        List<Post> postList = new ArrayList<>();
+        if (keyword.equals("")){
+            postList.addAll(postRepository.findAll());
+        }else {
+            postList.addAll(postRepository.findAllByTitleContainsOrCompanyCompanyNameContainsOrAddressContains(keyword,keyword,keyword));
+        }
+        if (cat_id == -1){
+            postList.addAll(postRepository.findAll());
+        }else {
+            postList.addAll(postRepository.findAllByCategoryId(cat_id));
+        }
+        if (city_id == -1){
+            postList.addAll(postRepository.findAll());
+        }else {
+            postList.addAll(postRepository.findAllByCityId(city_id));
+        }
+        if (salary1 == -1){
+            postList.addAll(postRepository.findAll());
+        }else {
+            postList.addAll(postRepository.findAllBySalary(salary1,salary2));
+        }
+        Set<Post> postSet = this.getDuplicate(postList,4);
+        List<Post> posts = new ArrayList<>();
+        for (Post post: postSet) {
+            posts.add(post);
+        }
+        sortList(posts);
+        return posts;
+    }
+    public Set<Post> getDuplicate(List<Post> postList, int number){
+//        Set<Post> posts = new HashSet<>();
+//        Set<Post> postSet = postList.stream().filter(n -> !posts.add(n)).collect(Collectors.toSet());
+        final Set<Post> postSet = new HashSet<>();
+
+        for (Post post : postList)
+        {
+            int occurrences = Collections.frequency(postList, post);
+
+            if (occurrences == number)
+            {
+                postSet.add(post);
+            }
+        }
+        return postSet;
+    }
+
+    @Override
+    public Page<PostResponse> findAllPage(Pageable pageable) {
+        Page<Post> postList = postRepository.findPosts(true,pageable);
+        List<PostResponse> postResponses = new ArrayList<>();
+        for (Post post: postList) {
+            postResponses.add(this.convert(post));
+        }
+        return new PageImpl<>(postResponses,pageable, postResponses.size());
+    }
+    public List<Post> sortList(List<Post> posts){
+        posts.sort((left, right) -> (int) (left.getId() - right.getId()));
+        return posts;
+    }
+
 }
