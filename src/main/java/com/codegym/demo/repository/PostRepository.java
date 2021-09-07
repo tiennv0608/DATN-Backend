74dfe8c6a0260f1ba6ab93538e3970a4f168e237
admin <dongdd159@gmail.com>
2021-09-07 18:05:45 +0700

    merge sprint1

--- src/main/java/com/codegym/demo/repository/PostRepository.java
+++ src/main/java/com/codegym/demo/repository/PostRepository.java
@@ -13,8 +13,6 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query(nativeQuery = true,value = "SELECT * FROM post p WHERE p.company_id =:id_company")
-
     @Query(nativeQuery = true, value = "SELECT * FROM post p WHERE p.company_id =:id_company")
     Iterable<Post> findAllByIdCompany(@Param("id_company") Long id);
     List<Post> findAllByTitleContainsOrCompanyCompanyNameContainsOrAddressContains(String keyword, String company, String address);
