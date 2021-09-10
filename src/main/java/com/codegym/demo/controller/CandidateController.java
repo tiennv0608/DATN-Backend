bb2088d6a4c929adbf10a4dd443eb53d5dba0d21
vuongvantu <vuongvantu2001x@gmail.com>
2021-09-10 08:32:37 +0700

    api-create-candidate-and-cv

---
+++ src/main/java/com/codegym/demo/controller/CandidateController.java
@@ -0,0 +1,31 @@
+package com.codegym.demo.controller;
+
+import com.codegym.demo.model.Candidate;
+import com.codegym.demo.service.candidate.ICandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/candidates")
+@CrossOrigin("*")
+public class CandidateController {
+@Autowired
+private ICandidateService iCandidateService;
+    @GetMapping
+    public ResponseEntity<Iterable<Candidate>> findAll() {
+        List<Candidate> cvs = (List<Candidate>) iCandidateService.findAll();
+        if (cvs.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(cvs, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Candidate> save(@RequestBody Candidate cv) {
+        return new ResponseEntity<>(iCandidateService.save(cv), HttpStatus.CREATED);
+    }
+
+}
---
+++ src/main/java/com/codegym/demo/service/candidate/CandidateService.java
@@ -0,0 +1,33 @@
+package com.codegym.demo.service.candidate;
+
+import com.codegym.demo.model.Candidate;
+import com.codegym.demo.repository.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CandidateService implements ICandidateService{
+    @Autowired
+    private CandidateRepository candidateRepository;
+    @Override
+    public Iterable<Candidate> findAll() {
+        return candidateRepository.findAll();
+    }
+
+    @Override
+    public Optional<Candidate> findById(Long id) {
+        return candidateRepository.findById(id);
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        return candidateRepository.save(candidate);
+    }
+
+    @Override
+    public void remove(Long id) {
+        candidateRepository.deleteById(id);
+    }
+}
