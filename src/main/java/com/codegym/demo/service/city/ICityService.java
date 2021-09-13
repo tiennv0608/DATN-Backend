0c45f287e867e2d6655a14c30fdca2f276acda38
vuongvantu <vuongvantu2001x@gmail.com>
2021-09-13 10:58:01 +0700

    fix-bug-null-city

--- src/main/java/com/codegym/demo/service/city/ICityService.java
+++ src/main/java/com/codegym/demo/service/city/ICityService.java
@@ -3,5 +3,7 @@ package com.codegym.demo.service.city;
 import com.codegym.demo.model.City;
 import com.codegym.demo.service.IGeneralService;
 
+import java.util.Optional;
+
 public interface ICityService extends IGeneralService<City> {
 }
